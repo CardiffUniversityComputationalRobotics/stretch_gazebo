4a92f2f087f1fcbcab2e87ea7b787c6d0a6c68a5
samuel <samuelmillannorman123@gmail.com>
2024-05-22 23:47:12 +0100

    Initial commit

---
+++ launch/spawn_robot.launch.py
@@ -0,0 +1,23 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    ld = LaunchDescription()
+
+    robot_entity = Node(
+        package="gazebo_ros",
+        executable="spawn_entity.py",
+        arguments=[
+            "-topic",
+            "/robot_description",
+            "-entity",
+            "stretch",
+        ],
+        output="screen",
+    )
+
+    ld.add_action(robot_entity)
+
+    return ld
